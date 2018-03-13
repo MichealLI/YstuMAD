8f6a8c16eb89a0c7de92ab98028efbfd6ba261d9
Ming <1178430600@qq.com>
2018-03-13 15:31:39 +0800

    Ming:第一次提交的新工程

---
+++ app/src/main/java/com/guangwai/project/ystumad/HomepageActivity.java
@@ -0,0 +1,13 @@
+package com.guangwai.project.ystumad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomepageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+    }
+}

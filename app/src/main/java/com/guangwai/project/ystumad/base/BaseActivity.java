32592f07c2b44dbf673fb5143521a991147aee6b
XiaoXue <18826103424@163.com>
2018-03-20 16:00:30 +0800

    XIAO XUE: 测试

--- app/src/main/java/com/guangwai/project/ystumad/base/BaseActivity.java
+++ app/src/main/java/com/guangwai/project/ystumad/base/BaseActivity.java
@@ -15,6 +15,6 @@ public class BaseActivity extends Activity {
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        int i = 1;
+        int i = 2;
     }
 }
